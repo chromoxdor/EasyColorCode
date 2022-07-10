654ad8ad3271c8b9af86b30999e8bde8ea5009db
chromoxdor <33860956+chromoxdor@users.noreply.github.com>
2022-07-10 19:11:37 +0200

    Update codemirror.js

--- codemirror.js
+++ codemirror.js
@@ -2,6 +2,7 @@
 // Distributed under an MIT license: https://codemirror.net/LICENSE
 
 // This is CodeMirror (https://codemirror.net), a code editor
+// implemented in JavaScript on top of the browser's DOM.
 //
 // You can find some technical background for some of the code below
 // at http://marijnhaverbeke.nl/blog/#cm-internals .
